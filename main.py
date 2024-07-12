40c6e787cab2767a849373f9794f81a7da8e1a2b
Mehar-Aziz <meharaziz.1614>
2024-07-12 22:57:00 +0500

    life in weeks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 age = input("Enter your age: ")
-as_as_int = int(age)
-days = 265
-weeks = 52
-months = 12
-print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+result= f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+print(result)
